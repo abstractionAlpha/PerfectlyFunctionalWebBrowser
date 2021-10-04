548d95a5acbb06119ea6d6abaa0ff51deddc231d
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-03 23:00:16 -0400

    [Implementation] Set up ThreadHandler with singleton.

--- perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/ThreadHandler.java
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/ThreadHandler.java
@@ -1,5 +1,31 @@
 package com.abstractionalpha.web.browser.perfectly_functional_web_browser;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Thread pool class. Implemented using an ArrayBlockingQueue.
+ * 
+ * @author Mason
+ *
+ */
 public class ThreadHandler {
+	
+	private static ThreadHandler instance;
+	
+	private ThreadHandler() {}
+	
+	public static ThreadHandler getInstance() {
+		if (instance == null)
+			instance = new ThreadHandler();
+		
+		return instance;
+	}
+	
+	public static ThreadHandler getInstance(int capacity) {
+		if (instance == null)
+			instance = new ThreadHandler(capacity);
+		
+		return instance;
+	}
 
 }
