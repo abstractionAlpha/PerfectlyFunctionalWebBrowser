f8c534a78bcc4caed9dfa25ba8549211a4e7e63c
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-04 11:58:44 -0400

    [Implementation] Created shell for PositionalLinkedList

---
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/util/list/positional/PositionalLinkedList.java
@@ -0,0 +1,76 @@
+package com.abstractionalpha.web.browser.perfectly_functional_web_browser.util.list.positional;
+
+import com.abstractionalpha.web.browser.perfectly_functional_web_browser.util.Position;
+
+public class PositionalLinkedList<E> implements PositionalList<E> {
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Position<E> first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> before(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> after(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> addFirst(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> addLast(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> addBefore(Position<E> p, E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Position<E> addAfter(Position<E> p, E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E set(Position<E> p, E e) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E remove(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private class PLLPosition<E> implements Position<E> {
+
+		public E getElement() throws IllegalStateException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+}
