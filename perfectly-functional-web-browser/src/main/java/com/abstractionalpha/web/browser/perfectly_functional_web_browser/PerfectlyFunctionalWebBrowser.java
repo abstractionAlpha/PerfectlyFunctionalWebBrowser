9b969115ebcb844613bffe65f142dc0917707d64
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-03 22:32:31 -0400

    [Implementation] Created main class

---
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/PerfectlyFunctionalWebBrowser.java
@@ -0,0 +1,9 @@
+package com.abstractionalpha.web.browser.perfectly_functional_web_browser;
+
+public class PerfectlyFunctionalWebBrowser {
+	
+	public static void main(String[] args) {
+		// TODO insert startup logic
+	}
+
+}
