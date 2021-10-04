ea9ba4c9e3d3eda7f0097e8c2e5ff06bc6f61139
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-03 23:25:45 -0400

    [Implementation] Implemented an early ThreadExecutor.

--- perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/ThreadHandler.java
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/ThreadHandler.java
@@ -1,9 +1,10 @@
 package com.abstractionalpha.web.browser.perfectly_functional_web_browser;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- * Thread pool class. Implemented using an ArrayBlockingQueue.
+ * Thread pool class.
  * 
  * @author abstractionAlpha
  *
@@ -12,14 +13,17 @@ public class ThreadHandler {
 	
 	private static ThreadHandler instance;
 	
-	private volatile ArrayBlockingQueue<Thread> queue;
+	private volatile LinkedBlockingQueue<Thread> waiting;
+	
+	private volatile ArrayBlockingQueue<Thread> running;
 	
 	private volatile int capacity;
 	
 	private static final int DEFAULT_CAPACITY = 5;
 	
 	private ThreadHandler(int capacity) {
-		queue = new ArrayBlockingQueue<Thread>(capacity);
+		waiting = new LinkedBlockingQueue<Thread>();
+		running = new ArrayBlockingQueue<Thread>(capacity);
 	}
 	
 	private ThreadHandler() {this(DEFAULT_CAPACITY);}
@@ -46,6 +50,49 @@ public class ThreadHandler {
 	 *
 	 */
 	private class ThreadExecutor extends Thread {
+		
+		public ThreadExecutor() {run();}
+		
+		/**
+		 * Uses polling to wait for something to be in the queue... again... and again... until the queue is no more
+		 */
+		public void run() {
+			
+			Thread curr;
+			
+			while (PerfectlyFunctionalWebBrowser.isRunning()) {
+				
+				/**
+				 * Case 1: Updating runqueue.
+				 * If the front thread is no longer alive, it will be removed.
+				 * After this, we will skip the remaining run of this loop.
+				 * This way we can loop indefinitely until all completed queues are finished.
+				 * 
+				 * TODO: Replace the running queue with a positional linked list.
+				 * That way, a thread can remove itself from the PLL when it is finished running.
+				 */
+				if (!running.peek().isAlive()) {
+					curr = running.remove();
+					continue;
+				}
+				
+				/**
+				 * Case 2: Adding to runqueue.
+				 * If there is space in the runqueue, then we can just add something to it.
+				 * If the runqueue is full or there's nothing in the waiting queue then we do not care.
+				 */
+				if (running.size() < capacity) {
+					curr = waiting.poll();
+					
+					if (curr != null) {
+						curr.run();
+						running.add(curr);
+					}
+					
+				}
+				
+			}
+		}
 		
 	}
 
