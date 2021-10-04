930f26b3f0593c3d59b0091535af14e3ca21db7c
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-04 12:07:14 -0400

    [Implementation] Wrote Node method

--- perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/util/list/positional/PositionalLinkedList.java
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/util/list/positional/PositionalLinkedList.java
@@ -65,10 +65,42 @@ public class PositionalLinkedList<E> implements PositionalList<E> {
 	}
 	
 	private static class Node<E> implements Position<E> {
+		
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+		
+		public Node(E element, Node<E> prev, Node<E> next) {
+			this.element = element;
+			this.prev = prev;
+			this.next = next;
+		}
 
 		public E getElement() throws IllegalStateException {
-			// TODO Auto-generated method stub
-			return null;
+			if (next == null)  // Works with sentinel node
+				throw new IllegalStateException("Position is no longer valid");
+			
+			return element;
+		}
+		
+		public Node<E> getPrev() {
+			return prev;
+		}
+		
+		public Node<E> getNext() {
+			return next;
+		}
+		
+		public void setElement(E element) {
+			this.element = element;
+		}
+		
+		public void setPrev(Node<E> prev) {
+			this.prev = prev;
+		}
+		
+		public void setNext(Node<E> next) {
+			this.next = next;
 		}
 		
 	}
