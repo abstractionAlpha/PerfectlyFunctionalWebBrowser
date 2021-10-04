af0ad4673a9968378721c680f4b2f663f078c530
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-03 23:31:43 -0400

    [Implementation] Created class for PositionalArrayList.
    This PAL will be used to handle the running queue. (Or, we will make it
    a running list!) This will be efficient because our self-aware list
    elements will be able to remove themselves when we are done.

---
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/util/PositionalArrayList.java
@@ -0,0 +1,13 @@
+package com.abstractionalpha.web.browser.perfectly_functional_web_browser.util;
+
+/**
+ * Positional ArrayList used to store our Threads in ThreadHandler.
+ * 
+ * @author abstractionAlpha
+ *
+ */
+public class PositionalArrayList {
+	
+	// TODO create PAL
+
+}
