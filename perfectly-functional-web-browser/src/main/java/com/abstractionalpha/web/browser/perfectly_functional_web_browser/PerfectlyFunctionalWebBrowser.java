5ffd40c4af4a165cbdb2e8846066fe009523d300
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-03 23:01:46 -0400

    [Implementation] Added a running boolean.

--- perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/PerfectlyFunctionalWebBrowser.java
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/PerfectlyFunctionalWebBrowser.java
@@ -2,8 +2,17 @@ package com.abstractionalpha.web.browser.perfectly_functional_web_browser;
 
 public class PerfectlyFunctionalWebBrowser {
 	
+	private static boolean running = false;
+	
 	public static void main(String[] args) {
 		// TODO insert startup logic
+		running = true;
+		
+		running = false;
+	}
+	
+	public static boolean isRunning() {
+		return running;
 	}
 
 }
