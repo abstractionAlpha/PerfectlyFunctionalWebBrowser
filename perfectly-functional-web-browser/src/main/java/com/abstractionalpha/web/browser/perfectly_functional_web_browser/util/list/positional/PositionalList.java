c24353dd76fe46efe3d8c656c9ff2f241fdad82a
abstractionAlpha <abstractionalpha@gmail.com>
2021-10-04 11:53:08 -0400

    Wrote PositionalList interface

---
+++ perfectly-functional-web-browser/src/main/java/com/abstractionalpha/web/browser/perfectly_functional_web_browser/util/list/positional/PositionalList.java
@@ -0,0 +1,31 @@
+package com.abstractionalpha.web.browser.perfectly_functional_web_browser.util.list.positional;
+
+import com.abstractionalpha.web.browser.perfectly_functional_web_browser.util.Position;
+
+public interface PositionalList<E> {
+	
+	int size();
+	
+	boolean isEmpty();
+	
+	Position<E> first();
+	
+	Position<E> last();
+	
+	Position<E> before(Position<E> p) throws IllegalArgumentException;
+	
+	Position<E> after(Position<E> p) throws IllegalArgumentException;
+	
+	Position<E> addFirst(E e);
+	
+	Position<E> addLast(E e);
+	
+	Position<E> addBefore(Position<E> p, E e);
+	
+	Position<E> addAfter(Position<E> p, E e);
+	
+	E set(Position<E> p, E e) throws IllegalArgumentException;
+	
+	E remove(Position<E> p) throws IllegalArgumentException;
+
+}
